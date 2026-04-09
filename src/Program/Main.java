67645ddbc4453e80bac385b043a4e4a8760ec999
Hendrick Miqueias <hendrickmiqueias@Hotmail.com>
2026-04-09 10:29:50 -0300

    Refactor return list instead print using predicates

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import Program.entities.Employee;
 
@@ -16,28 +17,26 @@ public class Main {
 		Locale.setDefault(Locale.of("pt", "BR"));
 		List<Employee> industryEmployeeList = new ArrayList<Employee>();
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		
-		
-		
+
 	}
+	//Print employee grouped by function
 	private static void printEmployeeGroupByFunction(Map<String, List<Employee>> employeeMap) {
-	    employeeMap.forEach((function, employees) -> {
-	        System.out.println("=== " + function + " ===");
-	        employees.forEach(System.out::println);
-	        System.out.println();
-	    });
+		employeeMap.forEach((function, employees) -> {
+			System.out.println("=== " + function + " ===");
+			employees.forEach(System.out::println);
+			System.out.println();
+		});
 	}
-	
-	private static void printByBirthMonth(List<Employee> employeeList, Integer... birthMonth) {
-	    List<Integer> months = Arrays.asList(birthMonth);
-	    employeeList.stream()
-	        .filter(p -> months.contains(p.getBirthDate().getMonthValue()))
-	        .forEach(System.out::println);
+
+	// Get employee List by birthMonth
+	private static List<Employee> getByBirthMonth(List<Employee> employeeList, Integer... birthMonth) {
+		List<Integer> months = Arrays.asList(birthMonth);
+		return employeeList.stream().filter(p -> months.contains(p.getBirthDate().getMonthValue()))
+				.collect(Collectors.toList());
 	}
-	
-	private static void printByAlphabeticalOrder(List<Employee> employeeList)
-	{
-		employeeList.stream().sorted(Comparator.comparing(Employee::getName)).forEach(System.out::println);
+
+	// Get employee list in alphabetical order
+	private static List<Employee> getByAlphabeticalOrder(List<Employee> employeeList) {
+		return employeeList.stream().sorted(Comparator.comparing(Employee::getName)).collect(Collectors.toList());
 	}
 }
